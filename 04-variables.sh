181b0d943c3fe73f93af0e5a431299a9265f827e
heman chandra <hemanchandra701@gmail.com>
2025-10-01 12:13:27 +0530

    heman

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1  :: Hi Sai,How are you?"
+echo "$PERSON2  :: I am fine $PERSON1  ,What about you?"
+echo "$PERSON1  :: Nothing,What are you doing?"
+echo "$PERSON2  :: I am watching Series,$PERSON1  "
