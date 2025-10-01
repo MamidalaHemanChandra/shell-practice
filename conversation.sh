312a3e63196e10f2210aae4dbcb8a749ad298432
heman chandra <hemanchandra701@gmail.com>
2025-10-01 12:07:57 +0530

    heman

---
+++ conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Heman  :: Hi Sai,How are you?"
+echo "Sai    :: I am fine Heman,What about you?"
+echo "Heman  :: Nothing,What are you doing?"
+echo "Sai    :: I am watching Series,Heman"
