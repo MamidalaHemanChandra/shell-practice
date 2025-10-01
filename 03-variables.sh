18fa0897b07614bdd8e8b27fb40ca7592712f96f
heman chandra <hemanchandra701@gmail.com>
2025-10-01 12:01:24 +0530

    heman

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,6 +2,7 @@
 
 PERSON1=Heman
 PERSON2=Sai
+
 echo "$PERSON1 : Hi $PERSON2, How are you?"
 echo "$PERSON2   : Hi $PERSON1, I am fine. How are you?"
 echo "$PERSON1 : I am good, What are you doing?"
