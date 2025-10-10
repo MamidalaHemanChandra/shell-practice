7c7ac65019002b5d72d2379bcdb65b0104a351d4
heman chandra <hemanchandra701@gmail.com>
2025-10-10 15:07:02 +0530

    backup

--- 20-backup.sh
+++ 20-backup.sh
@@ -10,9 +10,9 @@ Y="\e[33m"
 N="\e[0m"
 
 #/var/log/script/log.log
-FOLDER="/var/log/script"
-SCRIPT=$( echo $0 | cut -d "." -f1)
-FILE="$FOLDER/$SCRIPT.log"
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+LOGS_FILE="$FOLDER/$SCRIPT.log"
 
 mkdir -p $FOLDER
 echo -e "$G Script Started at $(date) $N" | tee -a $FILE
@@ -27,36 +27,38 @@ fi
 Source=$1
 Destination=$2
 Days=${3:-14}
+
 Usage(){
     echo "Arg passed are Source:$Source Destination:$Destination Days:$Days [Deafult is 14,Opational]"
+    exit 1
 }
 
 if [ $# -le 2 ];then
     Usage
 fi
 
-if [ ! $Source ];then
+if [ ! -d $Source ];then
     echo "Source:$Source doesn't exist"
     exit 1
 fi
 
-if [ ! $Destination ];then
+if [ ! -d $Destination ];then
     echo "Destination:$Destination doesn't exist"
     exit 1
 fi
 
-Log_files=$(find $Source -name "*.log" -type f -mtime +14)
+Log_files=$(find $Source -name "*.log" -type f -mtime +$Days)
+
 
 if [ -z $Log_files ];then
     echo "Log files are empty"
-    exit 1
 else
     echo "Log files found : $Log_files"
     TimeStamp=$(date +%F,%H:%M:%s)
-    Archieve="$Destination/app:$TimeStamp.zip"
+    Archieve="$Destination/app-logs-$TimeStamp.zip"
     echo "Archieve file is : $Archieve"
-    find $Source -name "*.log" -type f -mtime +14 | zip -@ -j $Archieve
-
+    find $Source -name "*.log" -type f -mtime +$Days | zip -@ -j $Archieve
+    
     if [ ! -f $Archieve ];then
         echo "Archieve Files not created"
         exit 1
