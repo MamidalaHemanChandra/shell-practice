c062fc5d336a8e7898643ca4288ec0640d3cf5e0
heman chandra <hemanchandra701@gmail.com>
2025-10-10 16:44:39 +0530

    corntab

---
+++ 21-corn.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+#0=Root, 1=normal user
+USERID=$(id -u)
+
+#colours
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+Source=$1
+Destination=$2
+Days=${3:-14}
+
+#/var/log/script/log.log
+FOLDER="/var/log/script"
+SCRIPT=$( echo $0 | cut -d "." -f1)
+FILE="$FOLDER/backup.log"
+
+mkdir -p $FOLDER
+echo -e "$G Script Started at $(date) $N" | tee -a $FILE
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R Take Root Access $N (Use Sudo)" 
+    exit 1
+fi
+
+Usage(){
+    echo "Arg passed are Source:$Source Destination:$Destination Days:[Deafult is 14,Opational]"
+    exit 1
+}
+
+if [ $# -lt 2 ];then
+    Usage
+fi
+
+if [ ! -d $Source ];then
+    echo "Source:$Source doesn't exist"
+    exit 1
+fi
+
+if [ ! -d $Destination ];then
+    echo "Destination:$Destination doesn't exist"
+    exit 1
+fi
+
+Log_files=$(find $Source -name "*.log" -type f -mtime +$Days)
+
+
+if [ -z "$Log_files" ];then
+    echo "Log files are empty"
+else
+    echo "Log files found : $Log_files"
+    TimeStamp=$(date +%F,%H:%M:%S)
+    Archieve="$Destination/app-logs-$TimeStamp.zip"
+    echo "Archieve file is : $Archieve"
+    find $Source -name "*.log" -type f -mtime +$Days | zip -@ -j $Archieve
+    
+    if [ ! -f $Archieve ];then
+        echo "Archieve Files not created"
+        exit 1
+    else
+        echo "$Archieve Files created"
+        while IFS= read -r line
+        do
+        rm -rf $line
+        echo "Deleted log files: $line"
+        done <<< $Log_files
+    fi
+
+    
+
+fi
