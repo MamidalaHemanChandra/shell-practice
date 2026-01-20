61e264beab0be9936cf1753654d6f6f999f8a134
heman chandra <hemanchandra701@gmail.com>
2026-01-20 18:31:32 +0530

    shell-script

---
+++ 02-coversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Sai:: Hi Heman, How are you?"
+echo "Heman:: Hello Sai, I am fine"
+echo "Sai:: What are you doing now?"
+echo "Heman:: I am learning DevOps"
---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1="Sai"
+Person2="Heman"
+
+echo "$Person1:: Hi $Person2, How are you?"
+echo "$Person2:: Hello $Person1, I am fine"
+echo "$Person1:: What are you doing $Person2?"
+echo "$Person2:: I am learning DevOps $Person1"
