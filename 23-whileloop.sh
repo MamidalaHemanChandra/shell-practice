4fa0d2148de4f3c02084b8fcacd8327edbb4f1c4
heman chandra <hemanchandra701@gmail.com>
2026-01-27 19:54:40 +0530

    shell-practice

--- 23-whileloop.sh
+++ 23-whileloop.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-count=5
+# count=5
 
-while [ $count -gt 0 ];
-do
-    echo "Time left: $count"
-    sleep 1
-    count=$(($count -1))
-
-done
-
-# while IFS=read -r file
+# while [ $count -gt 0 ];
 # do
-#     echo "Lines in a file: $file"
+#     echo "Time left: $count"
+#     sleep 1
+#     count=$(($count -1))
 
-# done < 20-script-1.sh
+# done
+
+while IFS=read -r file
+do
+    echo "Lines in a file: $file"
+
+done < 20-script-1.sh
