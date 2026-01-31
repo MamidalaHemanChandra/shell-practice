242f7e153b9358d7fc660f9369c188948448e4f0
heman chandra <hemanchandra701@gmail.com>
2026-01-31 09:14:16 +0530

    shell-practice

--- 26-disk-usage.sh
+++ 26-disk-usage.sh
@@ -5,14 +5,14 @@ IP_Address=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 Threshold=2
 Message=""
 
-while IFS=read -r line
+while IFS= read -r line
 do
     Usage=$(echo $line | awk '{print $6}'| cut -d "%" -f1)
     Parition=$(echo $line | awk '{print $7}' ) 
     if [ $Usage -ge $Threshold ];then 
         Message+="Hard Disk Usage is $Parition : $Usage % <br>"
     fi
-done
+done <<< $Disk_Usage
 
 echo -e "Message is: $Message"
 
